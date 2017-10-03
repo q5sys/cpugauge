a3dbb05a9b7757f7dd0762fcb0092afb5bf1b549
q5sys <jt@xsystems.com>
2017-10-03 14:37:35 -0400

    my changes from my local dir

--- main.cpp
+++ main.cpp
@@ -94,3 +94,49 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
+/*   stuff from sysadm to get cpu percentage to make this work in realtime - obviously not done yet
+ *
+QString result(){
+  QProcess proc;
+  QString command = "sysctl -n kern.cp_times"
+  proc.setProcessChannelMode(QProcess::MergedChannels); //need output
+  proc.start(command);
+  return QString(proc.readAllStandardOutput());
+}
+
+static QStringList last = QStringList();
+if(last.isEmpty()){
+  //need two ticks before it works properly
+  sleep(1);
+  last = result;
+  result = result();
+}
+
+double tot = 0;
+double roundtot;
+int cpnum = 0;
+for(int i=4; i<result.length(); i+=5){
+   //The values come in blocks of 5 per CPU: [user,nice,system,interrupt,idle]
+   cpnum++; //the number of CPU's accounted for (to average out at the end)
+   //qDebug() <<"CPU:" << cpnum;
+   long sum = 0;
+   //Adjust/all the data to correspond to diffs from the previous check
+   for(int j=0; j<5; j++){
+     QString tmp = result[i-j];
+     result[i-j] = QString::number(result[i-j].toLong()-last[i-j].toLong());
+     //need the difference between last run and this one
+     sum += result[i-j].toLong();
+     last[i-j] = tmp; //make sure to keep the original value around for the next run
+   }
+   QJsonObject vals;
+   roundtot = 100.0L - ( (100.0L*result[i].toLong())/sum );
+   tmp.setNum(qRound(roundtot));
+   vals.insert("busy", tmp );
+   tmp.setNum(cpnum);
+   retObject.insert("cpu" + tmp, vals);
+   //Calculate the percentage used for this CPU (100% - IDLE%)
+   tot += 100.0L - ( (100.0L*result[i].toLong())/sum ); //remember IDLE is the last of the five values per CPU
+ }
+
+*/
